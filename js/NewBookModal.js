c7385424b1afa5468262f4de994a034864ab7763
daniel hodgson <45772010+DanielCHodgson@users.noreply.github.com>
2025-02-03 20:08:28 +0000

    Modal form added with default behaviour prevented on submit TODO: improve validaiton

--- js/NewBookModal.js
+++ js/NewBookModal.js
@@ -2,8 +2,10 @@ export default class NewBookModal {
 
   constructor() {
     this.dialog = document.getElementById("new-book-modal");
+    this.form = document.getElementById("book-details");
     this.openButton = document.getElementById("add-book");
     this.closeButton = document.getElementById("modal-close-btn");
+    this.submitButton = document.getElementById("book-submit");
 
     this.bindEvents();
   }
@@ -17,17 +19,37 @@ export default class NewBookModal {
     this.dialog.close();
   }
 
+  submit(event) {
+    event.preventDefault();
+
+   
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const description = document.getElementById('description').value;
+
+    const authorPattern = /^[^0-9]*$/;
+    
+    if (!authorPattern.test(author)) {
+      alert("The author name cannot contain numbers.");
+      return; 
+    }
+
+
+    this.form.reset();
+    console.log("sumbitted!")
+  }
 
   bindEvents() {
 
-    this.openButton.addEventListener('click', () => this.open());
-    this.closeButton.addEventListener('click', () => this.close());
-    this.dialog.addEventListener('click', (event) => {
+    this.openButton.addEventListener("click", () => this.open());
+    this.closeButton.addEventListener("click", () => this.close());
+    this.submitButton.addEventListener("click", (event) => this.submit(event))
+    this.dialog.addEventListener("click", (event) => {
       if (event.target === this.dialog) {
+        
+        this.form.reset();
         this.close();
       }
     });
-
-
   }
 }
